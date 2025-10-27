0a68f126397a4bdcc91230ffbc4883096cadc8bd
Deepak singh <67883350+dkumarsing7@users.noreply.github.com>
2025-10-27 14:23:24 +0530

    update array using state

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -3,42 +3,31 @@ import { useState } from "react";
 
 // ..
 function UpdateObjectInState() {
-  const [car, setCar] = useState({
-    name: "BMW",
-    model: 2025,
-    seater: 3,
-  });
-const handleName=(e)=>{
-  setCar((car) => ({ ...car, name: e.target.value }))
-}
-const handleModel=(e)=>{
-  setCar((car) => ({ ...car, model: e.target.value }))
-}
-const handleSeater=(e)=>{
-  setCar((car) => ({ ...car, seater: e.target.value }))
-}
+  const [foods, setFoods] = useState([
+    "Apples",
+    "Oranges",
+    "Banana",
+    "Pineapple ",
+  ]);
+  const handleAdd = () => {
+    const currentFood = document.getElementById("currentFood").value;
+    document.getElementById("currentFood").value = "";
+    setFoods([...foods, currentFood]);
+  };
+  const handleRemove = (index) => {
+  const newArray = foods.filter((_, i)=>i!=index)
+    setFoods(newArray);
+  };
   return (
     <div>
-      <p>
-        My favourite car is : {car.name} {car.model} {car.seater} seater{" "}
-      </p>
-      <input
-        onChange={(e) =>handleName(e) }
-        type="text"
-        value={car.name}
-      />
-      <br />
-      <input
-        onChange={(e) => handleModel(e)}
-        type="number"
-        value={car.model}
-      />
-      <br />
-      <input
-        onChange={(e) => handleSeater(e)}
-        type="number"
-        value={car.seater}
-      />
+      <h1>Foods in Refrigerator to eat</h1>
+      <ul>
+        {foods.map((food, index) => (
+          <li key={index} onClick={()=>handleRemove(index)}>{food}</li>
+        ))}
+      </ul>
+      <input id="currentFood" type="text" placeholder="place in Refrigerator" />
+      <button onClick={handleAdd}>Add food</button>
     </div>
   );
 }
