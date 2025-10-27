1788ef20ae665f7171798fb5af98bcea02a8ed22
Deepak singh <67883350+dkumarsing7@users.noreply.github.com>
2025-10-27 18:58:33 +0530

    modify array's object

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,37 +1,55 @@
-// ..
+// ...
+
 import { useState } from "react";
 
-// ..
-function UpdateObjectInState() {
-  const [foods, setFoods] = useState([
-    "Apples",
-    "Oranges",
-    "Banana",
-    "Pineapple ",
+// ...
+
+function ChangeArrayObject() {
+  const [cars, setCars] = useState([
+    { name: "BMW", model: 2025, color: "Margenda" },
+    { name: "Mercedes", model: 2021, color: "venta Black" },
+    { name: "Royces Royal", model: 2023, color: "light-white" },
   ]);
-  const handleAdd = () => {
-    const currentFood = document.getElementById("currentFood").value;
-    document.getElementById("currentFood").value = "";
-    setFoods([...foods, currentFood]);
-  };
-  const handleRemove = (index) => {
-  const newArray = foods.filter((_, i)=>i!=index)
-    setFoods(newArray);
-  };
+
+  const [name, setName] = useState("");
+  const [model, setModel] = useState(new Date().getFullYear());
+  const [color, setColor] = useState("");
+
+
+const handleAdd=()=>{
+    const newCar = { name:name, model:model, color:color};
+    setCars([...cars, newCar]);
+    setName("");
+    setModel(new Date().getFullYear());
+    setColor("");
+}
   return (
-    <div>
-      <h1>Foods in Refrigerator to eat</h1>
-      <ul>
-        {foods.map((food, index) => (
-          <li key={index} onClick={()=>handleRemove(index)}>{food}</li>
-        ))}
-      </ul>
-      <input id="currentFood" type="text" placeholder="place in Refrigerator" />
-      <button onClick={handleAdd}>Add food</button>
-    </div>
+    <>
+      <div>
+        <h1>My favourite CARS list </h1>
+        <ul>
+          {cars.map((car, index) => (
+            <li key={index}>
+              {car.name} {car.model} {car.color}
+            </li>
+          ))}
+        </ul>
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="car name" />
+        <br />
+        <input type="number" value={model} onChange={(e)=>setModel(e.target.value)} placeholder="model name"/>
+        <br />
+        <input type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder="car color" />
+        <br />
+        <button onClick={handleAdd}>Add to List</button>
+      </div>
+    </>
   );
 }
 
 export default function App() {
-  return <UpdateObjectInState />;
+  return (
+    <>
+      <ChangeArrayObject />
+    </>
+  );
 }
