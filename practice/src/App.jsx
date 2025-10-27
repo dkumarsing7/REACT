34fb4d9af517cfc4214d3db9cd05047133f34622
Deepak singh <67883350+dkumarsing7@users.noreply.github.com>
2025-10-27 13:50:22 +0530

    update object in state

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,21 +2,47 @@
 import { useState } from "react";
 
 // ..
-function ColorPicker() {
-  const [color, setColor] = useState("lime");
-
+function UpdateObjectInState() {
+  const [car, setCar] = useState({
+    name: "BMW",
+    model: 2025,
+    seater: 3,
+  });
+const handleName=(e)=>{
+  setCar((car) => ({ ...car, name: e.target.value }))
+}
+const handleModel=(e)=>{
+  setCar((car) => ({ ...car, model: e.target.value }))
+}
+const handleSeater=(e)=>{
+  setCar((car) => ({ ...car, seater: e.target.value }))
+}
   return (
-    <div className="container">
-      <h1 className="heading">Color Picker</h1>
-      <div className="test-subject" style={{ backgroundColor: color }}>
-        This is your selected color
-      </div>
-      <h3 className="sub-heading">Select your color</h3>
-      <input type="color" onChange={(e) => setColor(e.target.value)} />
+    <div>
+      <p>
+        My favourite car is : {car.name} {car.model} {car.seater} seater{" "}
+      </p>
+      <input
+        onChange={(e) =>handleName(e) }
+        type="text"
+        value={car.name}
+      />
+      <br />
+      <input
+        onChange={(e) => handleModel(e)}
+        type="number"
+        value={car.model}
+      />
+      <br />
+      <input
+        onChange={(e) => handleSeater(e)}
+        type="number"
+        value={car.seater}
+      />
     </div>
   );
 }
 
 export default function App() {
-  return <ColorPicker />;
+  return <UpdateObjectInState />;
 }
